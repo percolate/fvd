4cfdc10a0f8935722db78ece5ce671beeb6adc49
Christopher Cliff <chris@percolate.com>
2014-12-30 16:53:32 -0600

    Updated dependencies to latest

--- test/parse.js
+++ test/parse.js
@@ -8,21 +8,21 @@ describe('parse', function(){
     it('should parse n4', function(){
         fvd.parse('n4').should.eql({
             'font-style': 'normal',
-            'font-weight': 400
+            'font-weight': '400'
         })
     })
 
     it('should parse i4', function(){
         fvd.parse('i4').should.eql({
             'font-style': 'italic',
-            'font-weight': 400
+            'font-weight': '400'
         })
     })
 
     it('should parse o4', function(){
         fvd.parse('o4').should.eql({
             'font-style': 'oblique',
-            'font-weight': 400
+            'font-weight': '400'
         })
     })
 
@@ -31,63 +31,63 @@ describe('parse', function(){
     it('should parse n1', function(){
         fvd.parse('n1').should.eql({
             'font-style': 'normal',
-            'font-weight': 100
+            'font-weight': '100'
         })
     })
 
     it('should parse n2', function(){
         fvd.parse('n2').should.eql({
             'font-style': 'normal',
-            'font-weight': 200
+            'font-weight': '200'
         })
     })
 
     it('should parse n3', function(){
         fvd.parse('n3').should.eql({
             'font-style': 'normal',
-            'font-weight': 300
+            'font-weight': '300'
         })
     })
 
     it('should parse n4', function(){
         fvd.parse('n4').should.eql({
             'font-style': 'normal',
-            'font-weight': 400
+            'font-weight': '400'
         })
     })
 
     it('should parse n5', function(){
         fvd.parse('n5').should.eql({
             'font-style': 'normal',
-            'font-weight': 500
+            'font-weight': '500'
         })
     })
 
     it('should parse n6', function(){
         fvd.parse('n6').should.eql({
             'font-style': 'normal',
-            'font-weight': 600
+            'font-weight': '600'
         })
     })
 
     it('should parse n7', function(){
         fvd.parse('n7').should.eql({
             'font-style': 'normal',
-            'font-weight': 700
+            'font-weight': '700'
         })
     })
 
     it('should parse n8', function(){
         fvd.parse('n8').should.eql({
             'font-style': 'normal',
-            'font-weight': 800
+            'font-weight': '800'
         })
     })
 
     it('should parse n9', function(){
         fvd.parse('n9').should.eql({
             'font-style': 'normal',
-            'font-weight': 900
+            'font-weight': '900'
         })
     })
 
